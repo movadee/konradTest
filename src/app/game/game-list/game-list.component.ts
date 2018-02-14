8003c69029ab43f321bdd2fc4913cea37d76d86f
Alla <alla@Allas-MacBook-Pro.local>
2018-02-14 15:08:17 -0500

    playing with dates

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -9,31 +9,49 @@ import { Game } from '../game';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
-  gameList: Game[];
+  gameList: Game[] = [];
   gameDataDirectory: string;
   favouriteTeam: string;
-  date;
+
+  // date;
   selectedDate;
+  displayDate;
+  // year;
+  // month;
+  // day;
 
   constructor(
     private gameService: GameService,
     private gameDataDirectoryService: GameDataDirectoryService) {
-    this.gameList = [];
+    this.gameList;
     this.favouriteTeam = 'Blue Jays';
+
+    this.selectedDate = new Date();
+    // this.displayDate = 
+    // this.date = new Date();
+    // this.year = this.date.getFullYear();
+    // this.month = this.date.getMonth() + 1;
+    // this.day = this.date.getDate();
+    // this.selectedDate = new Date(this.year, this.month-1, this.day);
+    // this.selectedDate = new Date("2018, 2, 2");
    }
 
   ngOnInit() {
-    // date setup
-    this.date = new Date();
-    this.selectedDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
-    
     this.gameDataDirectoryService.currentDataDirectory.subscribe(res => this.gameDataDirectory = res);
     this.getGameList();
     this.showFavTeamFirst(this.favouriteTeam);
   }
 
   getGameList(): void {
-    this.gameService.getGameList().subscribe(games => this.gameList = games);
+    let year = this.selectedDate.getFullYear();
+    let month = this.selectedDate.getMonth() + 1;
+    let day = this.selectedDate.getDate();
+
+    let apiFeed = 'http://gd2.mlb.com/components/game/mlb/year_' + year 
+                  + '/month_'+ (month < 10 ? '0' + month : month) 
+                  + '/day_' + (day < 10 ? '0' + day : day) + '/master_scoreboard.json';
+    this.gameService.getGameList(apiFeed).subscribe(games => this.gameList = games);
+    console.log(this.gameList);
   }
 
   updateGameDataDirectory(gameDataDirectory: string): void {
@@ -44,18 +62,21 @@ export class GameListComponent implements OnInit {
     if (this.gameList.length === 1) return;
 
     this.gameList.forEach((game, index) => {
-      (game.home_team_name === favTeam || game.away_team_name === favTeam) 
-      ? this.gameList.splice(index, 1, this.gameList.splice(1, 1, this.gameList[index])[0]) : '';
+      // (game.home_team_name === favTeam || game.away_team_name === favTeam) 
+      // ? this.gameList.splice(index, 1, this.gameList.splice(1, 1, this.gameList[index])[0]) : '';
     });
   }
 
-  nextDay() {
-    this.date.setDate(this.date.getDate() + 1);
-    this.selectedDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
-  } 
-  
-  previousDay() {
-    this.date.setDate(this.date.getDate() - 1);
-    this.selectedDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate());
+  changeDay(prevOrNext: string) {
+    // (prevOrNext === 'next') ? this.date.setDate(this.date.getDate() + 1) : this.date.setDate(this.date.getDate() - 1);
+    (prevOrNext === 'next') ? this.selectedDate.setDate(this.selectedDate.getDate() + 1) : this.selectedDate.setDate(this.selectedDate.getDate() - 1);
+    // this.year = this.date.getFullYear();
+    // this.month = this.date.getMonth();
+    // this.day = this.date.getDate();
+
+    // this.selectedDate = new Date(this.year, this.month-1, this.day);
+    // this.getGameList(this.year, this.month, this.day);
+    this.getGameList();
   }
+
 }
--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Game, GameListResponse } from './game';
 
 @Injectable()
@@ -9,13 +9,13 @@ export class GameService {
   
   private scoreboardUrl = 'http://gd2.mlb.com';
   private gamesListDirectory = '/components/game/mlb/year_2018/month_02/day_28/master_scoreboard.json';
+  private handleError: any;
 
   constructor(private http: HttpClient) { }
 
   /** GET games list from the server */
-  getGameList(): Observable<Game[]> {
-    const url = `${this.scoreboardUrl}${this.gamesListDirectory}`;
-    return this.http.get<GameListResponse>(url)
+  getGameList(apiFeed: string): Observable<Game[]> {
+    return this.http.get<GameListResponse>(apiFeed)
       .pipe(
         map(res => res['data'].games.game),
         map(games => Array.isArray(games) ? games : [games])
